590844287ac0d0fb963001e523cc266cd72859a0
Anjan Sunkara <Anjan Sunkara>
2015-11-08 16:09:17 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -13,7 +13,32 @@ NOTES:
 
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 void * sortedArrayPositionsChange(int *Arr, int len)
 {
+	int i, j, locations[2] = { 0, 0 };
+	if (Arr != NULL&&len >= 0)
+	{
+		for (i = 0; i < len - 1; i++)
+		{
+			if (Arr[i] > Arr[i + 1])
+			{
+				locations[0] = i; break;
+			}
+		}
+		for (j = i + 1; j < len - 1; j++)
+		{
+			if (Arr[j] > Arr[j + 1])
+			{
+				locations[1] = j + 1; break;
+			}
+		}
+		swap(&Arr[locations[0]], &Arr[locations[1]]);
+	}
 	return NULL;
 }
